90c8c73c218c208b04de05bc600c059d1e43b3c6
KimJeongDae22 <rlawjdeo0895@naver.com>
2025-04-29 17:26:47 +0900

    김정대 : 기본적인 캐릭터 움직임 구현
    추가로 간단히 캐릭터가 포함된 에셋을 준비

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    protected Rigidbody2D rigid2D;
+
+    [SerializeField] private SpriteRenderer playerRenderer;
+    [SerializeField] private Transform weaponPivot;
+
+    protected Vector2 moveDirection = Vector2.zero;
+
+    protected Vector2 knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
+    protected virtual void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+    protected virtual void Start()
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        InputKeys();
+    }
+    protected virtual void FixedUpdate()
+    {
+        Movement(moveDirection);
+        if (knockbackDuration > 0.0f)
+            knockbackDuration -= Time.deltaTime;
+    }
+    private void Movement(Vector2 direction)
+    {
+        direction = direction * 5;
+        if (knockbackDuration > 0.0f)
+        {
+            direction *= 0.2f;
+            direction += knockback;
+        }
+        rigid2D.velocity = direction;
+    }
+    protected virtual void InputKeys()
+    {
+
+    }
+}
